7ee04ec117808938f04dafe4b7a0969578181121
Omar Shibli <omarshib@gmail.com>
2012-11-11 01:04:45 +0200

    intial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='parsepy',
+    version='1.0.0',
+    description='ParsePy is a Python client for the Parse REST API',
+    long_description=open("README.mkd").read(),
+    packages=find_packages(),
+    author='omarshibli',
+    author_email='',
+    url="https://github.com/omarshibli/ParsePy.git",
+    license="GPL",
+    )
+
